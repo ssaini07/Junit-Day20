ba882e87d94498a93f6a175a80b587a1bc4ff80d
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-19 14:14:50 +0530

    performed test cases

--- src/main/java/com/bridgelabz/junit/UserRegistration.java
+++ src/main/java/com/bridgelabz/junit/UserRegistration.java
@@ -1,5 +1,95 @@
 package com.bridgelabz.junit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Check user first name with regex
+ */
 public class UserRegistration {
 
+	// method for checking firstName pattern
+	public boolean firstName(String firstName) {
+
+		
+		String regex = "^[A-Z][a-z]{3,}$";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(regex);
+		// Now create a matcher object
+		Matcher m = r.matcher(firstName);
+
+		if (m.matches()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean lastName(String lastName) {
+
+		// method for checking last Name pattern
+
+		String regex = "^[A-Z][a-z]{3,}$";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(regex);
+		// Now create a matcher object
+		Matcher m = r.matcher(lastName);
+
+		if (m.matches()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean emailCheck(String validEmail) {
+
+		// method for checking email address pattern
+
+		String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(regex);
+		// Now create a matcher object
+		Matcher m = r.matcher(validEmail);
+
+		if (m.matches()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean mobileChecker(String mobileFormat) {
+
+		// method for checking mobile format pattern
+
+		String regex = "^[0-9]{2}+[ ][0-9]{10}$";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(regex);
+		// Now create a matcher object
+		Matcher m = r.matcher(mobileFormat);
+
+		if (m.matches()) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean regexCheck(String password) {
+
+		// method for checking password format pattern
+
+		String regex = "^[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}";
+
+		// Create a Pattern object
+		Pattern r = Pattern.compile(regex);
+		// Now create a matcher object
+		Matcher m = r.matcher(password);
+
+		if (m.matches()) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,4 +1,36 @@
+import org.junit.Test;
+
+import com.bridgelabz.junit.UserRegistration;
+
+import junit.framework.Assert;
 
 public class UserRegistrationTest {
-
+	boolean result;
+	UserRegistration performTest = new UserRegistration();
+	@Test
+	public void firstNameTest() {
+		result = performTest.firstName("Shubham");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void lastNameTest() {
+		result = performTest.firstName("Saini");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void validEmailTest() {
+		result = performTest.firstName("abc.100@yahoo.com");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void mobileNumberChecker() {
+		result = performTest.firstName("98475683982983");
+		Assert.assertEquals(true, result);
+	}
+	@Test
+	public void passwordChecker() {
+		result = performTest.firstName("345shHjk&^#");
+		Assert.assertEquals(true, result);
+	}
+	
 }
